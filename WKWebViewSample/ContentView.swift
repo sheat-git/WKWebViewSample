3c78ce789ab4fc09c97b9d83b6c4128b71e2ecf1
sheat-git <sheat.ohata@gmail.com>
2022-04-16 11:21:05 +0900

    Initial Commit

---
+++ WKWebViewSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WKWebViewSample
+//
+//  Created by 大畑允人 on 2022/04/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WKWebViewSample/WKWebViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKWebViewSampleApp.swift
+//  WKWebViewSample
+//
+//  Created by 大畑允人 on 2022/04/16.
+//
+
+import SwiftUI
+
+@main
+struct WKWebViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
